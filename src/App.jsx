2b478950aba2782cd5b9668eb7a22d9a945993e6
macrineakinyi84 <akinyi.macrine.o@gmail.com>
2025-11-04 20:58:57 +0300

    Intial scaffold:Express + Prisma backend,frontend scaffold

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+﻿import React from 'react'
+
+export default function App() {
+  return (
+    <div className='p-8'>
+      <h1 className='text-2xl font-bold'>HuruDrive</h1>
+      <p className='mt-4 text-gray-600'>If you see this, the Vite dev server is serving files correctly.</p>
+    </div>
+  )
+}
